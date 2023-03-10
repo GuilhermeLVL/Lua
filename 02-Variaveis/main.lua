49cc96836bc78d4678294ef37ae9ab0e7b8302b2
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-03-10 00:35:51 -0300

    Create main.lua

---
+++ 02-Variaveis/main.lua
@@ -0,0 +1,12 @@
+num1 = 10
+nome = "Guilherme"
+Valor = 5.2
+logica= true
+soma = 12+3
+
+
+print( "Tipo:",type(num1),"Valor:",num1)
+print(  "Tipo:",type(nome),"Valor:",nome)
+print(  "Tipo:",type(Valor),"Valor:",Valor)
+print(  "Tipo:",type(logica),"Valor:",logica)
+print(soma)
